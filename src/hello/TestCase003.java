6511b4c896c6c37e7919cc54cc32b9e654ce410f
agirb <agirbasomer35@gmail.com>
2022-04-27 16:51:26 +0300

    creating conflict

---
+++ src/hello/TestCase003.java
@@ -0,0 +1,9 @@
+package hello;
+
+public class TestCase003 {
+    //this is unknown chanege
+    //taht exist in master
+    //now git not know to do with this
+    //when we merge and conflict
+
+}
